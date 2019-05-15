8ccea0263659d7f1692d3d07374bbff10d08ccf1
Gabriel Vidal <golobar2012@gmail.com>
2019-05-15 19:09:38 +0200

    Segunda modificacion

---
+++ src/Usuario.java
@@ -0,0 +1,55 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Usuario {
+    private final String codigo;
+    private final String nombre;
+    private final String apellidos;
+    private final String correo;
+    private final String fechaNac;
+
+    Map<String,Usuario> user=new TreeMap<>();
+
+    public Usuario(String codigo, String nombre, String apellidos, String correo, String fechaNac) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.fechaNac = fechaNac;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public String info() {
+        String salida="";
+
+        salida+="Nombre completo: "+nombre+" "+apellidos+"\n";
+        salida+="Codigo de usuario: "+codigo+"\n";
+        salida+="Correo electronico: "+correo+"\n";
+        salida+="Fecha de nacimiento "+fechaNac;
+
+        return salida;
+    }
+
+    public void creaUsuario(Usuario u){
+         user.put(codigo,u);
+    }
+}
--- src/Ventana.java
+++ src/Ventana.java
@@ -1,10 +1,97 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana {
-    public static void main(String[] args) {
-        JFrame ventana=new JFrame("Ventana de la practica final");
 
-        ventana.setBounds(50,50,500,500);
+    private static JTextField contrasenya;
+
+    public static void main(String[] args) {
+        JFrame ventana=new JFrame("Inicio");
+        ventana.setLayout(new FlowLayout());
+        JPanel acceso=new JPanel();
+        JPanel datosAcceso=new JPanel();
+        contrasenya=new JTextField(13);
+        JLabel textoContrasenya=new JLabel("Contraseña");
+        datosAcceso.add(textoContrasenya);
+        datosAcceso.add(contrasenya);
+
+        JButton entrar=new JButton("Entrar");
+        acceso.add(entrar);
+
+        datosAcceso.add(acceso);
+        ventana.add(datosAcceso);
+        ventana.add(acceso);
+
+
+
+
+        entrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        JButton nuevo=new JButton("¿Eres nuevo? Registrate ahora");
+
+
+        ventana.add(nuevo);
+
+        nuevo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame creacionUsuario=new JFrame();
+                creacionUsuario.setLayout(new GridLayout(2,0,15,10));
+
+                JPanel datosUsuario=new JPanel(new GridLayout(0,2,5,5));
+
+                JLabel textoNombre=new JLabel("Nombre");
+                JLabel textoApellidos=new JLabel("Apellidos");
+                JLabel textoCorreo=new JLabel("Correo electronico");
+                JLabel textoFecha=new JLabel("Fecha de nacimiento");
+
+                JTextField nombre=new JTextField(10);
+                JTextField apellidos=new JTextField(10);
+                JTextField correo=new JTextField(10);
+                JTextField fechaNac=new JTextField(10);
+
+
+                datosUsuario.add(textoNombre);
+
+                datosUsuario.add(nombre);
+
+                datosUsuario.add(textoApellidos);
+
+                datosUsuario.add(apellidos);
+
+                datosUsuario.add(textoCorreo);
+
+                datosUsuario.add(correo);
+
+                datosUsuario.add(textoFecha);
+
+                datosUsuario.add(fechaNac);
+
+                creacionUsuario.add(datosUsuario);
+
+                JPanel botonAceptar=new JPanel(new FlowLayout());
+                JButton aceptar=new JButton("Aceptar");
+
+                botonAceptar.add(aceptar);
+
+                creacionUsuario.add(botonAceptar);
+
+                creacionUsuario.setBounds(50,50,250,250);
+                creacionUsuario.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                creacionUsuario.setLocationRelativeTo(null);
+                creacionUsuario.setVisible(true);
+            }
+        });
+
+        ventana.setBounds(50,50,250,250);
         ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         ventana.setLocationRelativeTo(null);
         ventana.setVisible(true);
