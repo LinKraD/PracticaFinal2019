2a98aea8824a63a57032edb7d70226c272e0cb8d
Miguel-David Orrico Teruel <otm@iesseveroochoa.net>
2019-05-16 15:29:27 +0200

    Añadido programa de prueba

---
+++ src/PruebaUsuario.java
@@ -0,0 +1,8 @@
+public class PruebaUsuario{
+    public static void main(String[] args) {
+        PanelUsuario panelUsuario=new PanelUsuario();
+        panelUsuario.mostrar();
+        Usuario nuevo=panelUsuario.getUsuario();
+        System.out.println(nuevo);
+    }
+}
